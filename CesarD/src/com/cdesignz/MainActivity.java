dc4dd02e0eae97d66c63666fa4891569b86f4ccd
César Silva <cesarsilvascp9@hotmail.com>
2012-10-04 17:57:27 +0200

    And

---
+++ CesarD/src/com/cdesignz/MainActivity.java
@@ -0,0 +1,28 @@
+
+package com.cdesignz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     
+    } 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+
+public void sendMessage(View view) {
+   // Do Something *//
+		}
+}
---
+++ workspaceBackup/gen/com/dgd/gfdsrg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dgd.gfdsrg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
